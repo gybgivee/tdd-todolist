535ea6149ec4f1f527f250be75692a1e31748748
gybgivee <kmcn0512@gmail.com>
2022-05-30 18:31:08 +0100

    done-but-need-ui

--- spec/TodoList.spec.js
+++ spec/TodoList.spec.js
@@ -1,17 +1,113 @@
 const TodoList = require("../src/TodoList.js")
-
+//let todoList = new TodoList();
 describe("TodoList", () => {
+
   it("creates a todo item", () => {
     // set up
     const todoList = new TodoList()
-    const expected = {
+    const expected = [
+      {
       id: 1,
       text: "turn the heating on!",
       status: "incomplete"
-    }
+    }]
     // execute
-    const result = todoList.create("turn the heating on!")
+    todoList.create("turn the heating on!")
     // verify
-    expect(result).toEqual(expected)
+    expect(todoList.toDoList).toEqual(expected)
+  })
+  it("create list of to do items", () => {
+    const todoList = new TodoList()
+    const expected =[
+      {
+        id: 1,
+        text: "turn the heating on!",
+        status: "incomplete"
+      },
+      {
+        id: 2,
+        text: "Write the test first!",
+        status: "incomplete"
+      },
+      {
+        id: 3,
+        text: "Crack on with JS",
+        status: "incomplete"
+      }
+    ]
+    // execute
+    todoList.create("turn the heating on!")
+    todoList.create("Write the test first!");
+    todoList.create("Crack on with JS");
+    console.log('todoList :'+todoList);
+    // verify
+    expect(todoList.toDoList).toEqual(expected)
+  })
+  it("Set Complete by Id",()=>{
+    const todoList = new TodoList()
+    const expected = [
+      {
+      id: 1,
+      text: "turn the heating on!",
+      status: "complete"
+    }]
+    todoList.create("turn the heating on!")
+    todoList.setComplete(1)
+    expect(todoList.toDoList).toEqual(expected)
+  })
+  it("Set Complete by Id for non-exist object",()=>{
+    const todoList = new TodoList()
+    const expected ='Todo item not found'
+    expect(todoList.setComplete(1)).toEqual(expected)
+  })
+  it("Remove a to do item by Id",()=>{
+    const todoList = new TodoList()
+    const expected =[
+      {
+        id: 2,
+        text: "Write the test first!",
+        status: "incomplete"
+      },
+      {
+        id: 3,
+        text: "Crack on with JS",
+        status: "incomplete"
+      }
+    ]
+    // execute
+    todoList.create("turn the heating on!")
+    todoList.create("Write the test first!");
+    todoList.create("Crack on with JS");
+    expect(todoList.removeById(1)).toEqual(expected)
+  })
+  it("Remove non-exist to do item by Id",()=>{
+    const todoList = new TodoList()
+    const expected ='Todo item not found'
+    // execute
+    expect(todoList.removeById(1)).toEqual(expected)
+  })
+  it("Get All list of to do items",()=>{
+    const todoList = new TodoList()
+    const expected =[
+      {
+        id: 1,
+        text: "turn the heating on!",
+        status: "complete",
+      },
+      {
+        id: 2,
+        text: "Write the test first!",
+        status: "incomplete"
+      }
+    ]
+    // execute
+    todoList.create("turn the heating on!")
+    todoList.create("Write the test first!");
+    todoList.create("Crack on with JS");
+
+    todoList.setComplete(1)
+    todoList.removeById(3)
+    // verify
+    expect(todoList.toDoList).toEqual(expected)
   })
 })
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,81 @@
 class TodoList {
+    constructor(toDoList = []) {
+        this.toDoList = toDoList;
+    }
+    create(myList) {
+        let myNewToDo = this.toDoList;
+        console.log(this.isEmptyObject(myNewToDo));
+        let lastIndex;
 
+        if (this.isEmptyObject(myNewToDo)) {
+            lastIndex = 0;
+            Object.assign(myNewToDo.push({
+                id: 1,
+                text: myList,
+                status: 'incomplete'
+            })
+            );
+
+        } else {
+            console.log(myNewToDo.length);
+            lastIndex = Object.keys(myNewToDo).length;
+            console.log('lastIndex ' + lastIndex);
+            Object.assign(myNewToDo.push(
+                {
+                    id: lastIndex + 1,
+                    text: myList,
+                    status: 'incomplete'
+                }));
+
+        }
+        return myNewToDo
+
+    }
+    setComplete(id) {
+        console.log('toDoList',this.toDoList);
+        let myNewToDo = this.toDoList;
+        let resetList = myNewToDo.find(element => element.id === id);
+        if(resetList){
+            resetList.status = 'complete';
+            return resetList
+        }else{
+            return 'Todo item not found'
+        }
+    }
+    removeById(id){
+        console.log('toDoList',this.toDoList);
+        let myNewToDo = this.toDoList;
+        //const removeList = myNewToDo.filter((element) => element.id !== id);
+        //console.log(removeList);
+        const listFinder = myNewToDo.find(element => element.id === id);
+        console.log('listFinder ',listFinder);
+        if(listFinder){
+        const newIndex = myNewToDo.indexOf(listFinder)
+        myNewToDo.splice(newIndex,1);
+        
+        return myNewToDo;
+        }else{
+            return 'Todo item not found'
+        }
+       
+    }
+    getAll(){
+        return this.toDoList;
+    }
+
+    isEmptyObject(object) {
+
+        return Object.keys(object).length === 0;
+    }
 }
+const myToDoList = new TodoList();
+myToDoList.create('Hello')
+myToDoList.create('OK');
+myToDoList.create('Ola');
+
+console.log('myToDoList', myToDoList);
+myToDoList.setComplete(1);
+myToDoList.removeById(3);
+
 
 module.exports = TodoList
